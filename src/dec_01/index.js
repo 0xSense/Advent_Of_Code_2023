96ac197fee12229763f05ef9e64deef50221eb63
Sense <105939929+0xSense@users.noreply.github.com>
2023-12-04 11:33:20 -0500

    chore: created a array for the document  of calibration values

--- src/dec_01/index.js
+++ src/dec_01/index.js
@@ -1,6 +1,9 @@
-// December 1st advent of Code 
-// Snow Production 
-// 50 locations
+/* December 1st advent of Code 
+ Snow Production 
+ 50 locations
+ calibration valued are record by 
+ receiving the first digit and the last digit in a string
+*/
 
 function sum(a, b) {
     return a + b;
